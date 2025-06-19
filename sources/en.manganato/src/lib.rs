3cb2914323270c043f56eb57fa54dd00da1f125d
kantan <31490942+kkantan@users.noreply.github.com>
2025-06-19 16:42:05 -0500

    feat: add mangabox template (#5)

---
+++ sources/en.manganato/src/lib.rs
@@ -0,0 +1,27 @@
+#![no_std]
+use aidoku::{prelude::*, DeepLinkHandler, Home, ImageRequestProvider, Source};
+use mangabox::{Impl, MangaBox, Params};
+
+const BASE_URL: &str = "https://www.manganato.gg";
+
+struct MangaNato;
+
+impl Impl for MangaNato {
+	fn new() -> Self {
+		Self
+	}
+
+	fn params(&self) -> Params {
+		Params {
+			base_url: BASE_URL.into(),
+			..Default::default()
+		}
+	}
+}
+
+register_source!(
+	MangaBox<MangaNato>,
+	Home,
+	ImageRequestProvider,
+	DeepLinkHandler
+);
---
+++ sources/en.manganelo/src/lib.rs
@@ -0,0 +1,27 @@
+#![no_std]
+use aidoku::{prelude::*, DeepLinkHandler, Home, ImageRequestProvider, Source};
+use mangabox::{Impl, MangaBox, Params};
+
+const BASE_URL: &str = "https://www.nelomanga.net";
+
+struct MangaNelo;
+
+impl Impl for MangaNelo {
+	fn new() -> Self {
+		Self
+	}
+
+	fn params(&self) -> Params {
+		Params {
+			base_url: BASE_URL.into(),
+			..Default::default()
+		}
+	}
+}
+
+register_source!(
+	MangaBox<MangaNelo>,
+	Home,
+	ImageRequestProvider,
+	DeepLinkHandler
+);
